07df723d766900ba14c5a15370296f1ae35bcf80
Richard Dallaway <richard@dallaway.com>
2010-10-14 15:19:11 +0100

    Added Eclipsity plugin. http://github.com/musk/SbtEclipsify

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -1,8 +1,8 @@
-
-
 import sbt._
+import de.element34.sbteclipsify._
 
-class LiftProject(info: ProjectInfo) extends DefaultWebProject(info) {
+
+class LiftProject(info: ProjectInfo) extends DefaultWebProject(info) with Eclipsify {
   val mavenLocal = "Local Maven Repository" at
   "file://"+Path.userHome+"/.m2/repository"
 
