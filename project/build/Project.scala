bcd0608e9543021dd8abc0abaafb81bf96b01445
Richard Dallaway <richard@dallaway.com>
2012-05-26 17:32:59 +0100

    Using lift modules version of widgets.

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -7,12 +7,13 @@ class LiftProject(info: ProjectInfo) extends DefaultWebProject(info) with Eclips
   val scalatoolsSnapshot = "Scala Tools Snapshot" at "http://scala-tools.org/repo-snapshots/"
   val scalatoolsRelease = "Scala Tools Snapshot" at "http://scala-tools.org/repo-releases/"
 
-  val liftVersion = "2.1"
+  val liftVersion = "2.4"
 
   override def libraryDependencies = Set(
     "net.liftweb" %% "lift-webkit" % liftVersion % "compile->default" withSources(),
     "net.liftweb" %% "lift-testkit" % liftVersion % "compile->default",
-    "net.liftweb" %% "lift-widgets" % liftVersion % "compile->default",
+    //"net.liftweb" %% "lift-widgets" % liftVersion % "compile->default",
+    "net.liftmodules" %% "widgets" % (liftVersion+"-1.0-SNAPSHOT") % "compile->default",
     "org.mortbay.jetty" % "jetty" % "6.1.22" % "test->default",
     "junit" % "junit" % "4.5" % "test->default",
     "org.scala-tools.testing" % "specs" % "1.6.1" % "test->default"
